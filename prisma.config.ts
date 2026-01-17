78fc1198360aa4a7fd255dab231345b2ef124627
Roberto Leonel <leotufe@gmail.com>
2026-01-17 19:02:28 -0400

    Update backend

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,8 +1,16 @@
+import "dotenv/config";
 import { defineConfig } from "prisma/config";
+
+const DATABASE_URL = process.env.DATABASE_URL;
+if (!DATABASE_URL) {
+  throw new Error("DATABASE_URL is missing in environment");
+}
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
   datasource: {
-    url: process.env.DATABASE_URL!,
+    db: {
+      url: DATABASE_URL,
+    },
   },
 });
