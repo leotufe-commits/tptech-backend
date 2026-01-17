b187e6e24924698249f2aa73281155fa1cedc706
Roberto Leonel <leotufe@gmail.com>
2026-01-17 16:50:55 -0400

    fix(prisma): set datasource.url for migrate deploy

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,16 +1,8 @@
-import "dotenv/config";
 import { defineConfig } from "prisma/config";
-
-const DATABASE_URL = process.env.DATABASE_URL;
-if (!DATABASE_URL) {
-  throw new Error("DATABASE_URL is missing in environment");
-}
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
   datasource: {
-    db: {
-      url: DATABASE_URL,
-    },
+    url: process.env.DATABASE_URL!,
   },
 });
